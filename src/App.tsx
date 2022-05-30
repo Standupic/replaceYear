f0b867725b3935a1c8383b006da94a4ce33f7b69
gkutateladze <georgerailz@gmail.com>
2022-05-30 17:31:25 +0300

    feat: [master] init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Preloader } from 'juicyfront';
 import { Route, Switch } from 'react-router-dom';
 import ModalContainer from './components/common/Modal/Modal';
@@ -8,12 +8,21 @@ import NavigationTabs from './components/common/NavigationTabs';
 import MainTittle from './components/common/MainTittle';
 import User from './components/common/User';
 import { Stack, Box } from './components/styledComponents';
-import useHelperList from './hooks/useHelperList';
 import EntryPoint from './components/common/EntryPoint';
 const Applications = React.lazy(() => import('./pages/applications'));
 const CreateApplication = React.lazy(() => import('./pages/createApplication'));
 
 const App = () => {
+  useEffect(() => {
+    console.warn('================================================================');
+    console.log(
+      `%c Активное приложение ${process.env.REACT_APP_NAME}`,
+      'color:DodgerBlue;font-size:large',
+    );
+    console.log(`%c ${process.env.REACT_APP_V}`, 'color:red;font-size:large');
+    console.warn('================================================================');
+  }, []);
+
   useAuthorization();
   // useHelperList();
   return (
