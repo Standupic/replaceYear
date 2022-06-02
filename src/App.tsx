ebf7a0ec0e068045dffbd45fb012a65bc150d67f
vshashinov <satndupic87@gmail.com>
2022-06-02 18:16:59 +0300

    feat: delete env variable from middlewears

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect } from 'react';
 import { Preloader } from 'juicyfront';
-import {Route, Switch, useLocation} from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ModalContainer from './components/common/Modal/Modal';
 import './index.scss';
 import useAuthorization from './hooks/useAuthorization';
@@ -26,7 +26,6 @@ const App = () => {
   useAuthorization();
   const location = useLocation();
   console.log(location);
-  // useHelperList();
   return (
     <Box width={'880px'}>
       <EntryPoint>
@@ -43,7 +42,7 @@ const App = () => {
           <Route path="/service/replaceyears" exact>
             <Suspense fallback={<Preloader />}>{<CreateApplication />}</Suspense>
           </Route>
-          <Route path="/service/applications">
+          <Route path="/service/applications" exact>
             <Suspense fallback={<Preloader />}>
               <Applications />
             </Suspense>
--- src/middlewares/user.ts
+++ src/middlewares/user.ts
@@ -3,10 +3,10 @@ import Axios from '../api/interceptor';
 
 export const getUser = createAsyncThunk('users/getUser', async (_, { rejectWithValue }) => {
   try {
-    if (process.env['REACT_APP_GET_USER']) {
-      const user = await Axios.get(process.env['REACT_APP_GET_USER']);
-      return user.data;
-    }
+    const user = await Axios.get(
+      'sap/opu/odata4/sap/zhrbc/default/sap/zhrbc_0720_react_utils/0001/IUser(%270%27)',
+    );
+    return user.data;
   } catch (e) {
     return rejectWithValue('Не удалось получить пользователя!');
   }
@@ -14,18 +14,17 @@ export const getUser = createAsyncThunk('users/getUser', async (_, { rejectWithV
 
 export const getToken = createAsyncThunk('user/getToken', async (_, { rejectWithValue }) => {
   try {
-    if (process.env['REACT_APP_GET_TOKEN']) {
-      const { headers } = await Axios.get(process.env['REACT_APP_GET_TOKEN'], {
+    const { headers } = await Axios.get(
+      'sap/opu/odata4/sap/zlocal_service/default/sap/zhrbc_0720_react_utils/0001/IXCSRFToken',
+      {
         headers: {
           'X-CSRF-Token': 'Fetch',
           // @ts-ignore
           ['cacheURL']: true,
         },
-      });
-      if (headers) {
-        return headers['x-csrf-token'];
-      }
-    }
+      },
+    );
+    return headers['x-csrf-token'];
   } catch (e) {
     return rejectWithValue('Не удалось получить токен!');
   }
@@ -35,12 +34,10 @@ export const getPermissions = createAsyncThunk(
   'user/permissions',
   async (_, { rejectWithValue }) => {
     try {
-      if (process.env['REACT_APP_GET_USER_PERMISSIONS']) {
-        const { data } = await Axios.get(process.env['REACT_APP_GET_USER_PERMISSIONS']);
-        if (data) {
-          return data.value;
-        }
-      }
+      const { data } = await Axios.get(
+        'sap/opu/odata4/sap/zhrbc/default/sap/zhrbc_0720_react_utils/0001/IService',
+      );
+      return data.value;
     } catch (e) {
       return rejectWithValue('Не удалось получить доступы!');
     }
